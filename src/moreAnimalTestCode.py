360005250571e865c8a9c677a26cddbefdf447ae
Jeong JaeIn <jaein1608@gmail.com>
2023-09-06 00:41:15 +0900

    Create moreAnimalTestCode.py

---
+++ src/moreAnimalTestCode.py
@@ -0,0 +1,38 @@
+
+import cv2
+import cvlib as cv
+from cvlib.object_detection import draw_bbox
+import pafy
+
+url = 'https://www.youtube.com/watch?v=VsBdl9tFhFk'
+video = pafy.new(url)
+best_stream = video.getbest()
+cap = cv2.VideoCapture()
+cap.open(best_stream.url)
+
+target_classes = ['cat', 'dog', 'bird', 'horse', 'elephant', 'sheep', 'cow', 'giraffe', 'zebra', 'bear']
+
+while True:
+    ret, frame = cap.read()
+
+    bbox, label, conf = cv.detect_common_objects(frame)
+
+    selected_bbox = []
+    selected_label = []
+    selected_conf = []
+
+    for idx, obj_label in enumerate(label):
+        if obj_label in target_classes:
+            selected_bbox.append(bbox[idx])
+            selected_label.append(obj_label)
+            selected_conf.append(conf[idx])
+
+    output_image = draw_bbox(frame, selected_bbox, selected_label, selected_conf)
+
+    cv2.imshow('Object Detection', output_image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
