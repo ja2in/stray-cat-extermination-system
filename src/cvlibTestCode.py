8aacde18a6eb5deb4fd930e7fadd43f06dc5dbd9
Jeong JaeIn <jaein1608@gmail.com>
2023-09-06 00:39:35 +0900

    Update cvlibTestCode.py

--- src/cvlibTestCode.py
+++ src/cvlibTestCode.py
@@ -3,23 +3,17 @@ import cvlib as cv
 from cvlib.object_detection import draw_bbox
 import pafy
 
-# YouTube 동영상 URL
 url = 'youtube link'
 
-# YouTube 동영상 가져오기
 video = pafy.new(url)
 best_stream = video.getbest()
 cap = cv2.VideoCapture()
 cap.open(best_stream.url)
 
 while True:
-    # 비디오 프레임 읽기
     ret, frame = cap.read()
-
-    # 객체 탐지 수행
     bbox, label, conf = cv.detect_common_objects(frame)
 
-    # 고양이 클래스만 선택
     selected_bbox = []
     selected_label = []
     selected_conf = []
@@ -30,16 +24,12 @@ while True:
             selected_label.append(obj_label)
             selected_conf.append(conf[idx])
 
-    # 감지된 고양이 객체에 사각형 및 레이블 그리기
     output_image = draw_bbox(frame, selected_bbox, selected_label, selected_conf)
 
-    # 화면에 프레임 표시
     cv2.imshow('Cat Detection', output_image)
-
-    # 'q' 키를 누르면 종료
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# 리소스 해제
 cap.release()
 cv2.destroyAllWindows()
